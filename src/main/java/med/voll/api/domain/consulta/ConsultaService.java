5b842ced1d5bb13c94a616ade1ad96c1bafc54ab
amandabomfim <amandabomfimdeoliveiraa@gmail.com>
2026-06-19 09:26:29 -0300

    Implementando as regras de negocio de agendamento.

--- src/main/java/med/voll/api/domain/consulta/ConsultaService.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaService.java
@@ -9,8 +9,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @Service
@@ -25,13 +26,36 @@ public class ConsultaService {
     PacienteRepository pacienteRepository;
 
     public void agendar(DadosAgendamentoConsulta dados) {
-//        Medico medico = null;
-//
-//        if (dados.idMedico() != null){
-//             medico = medicoRepository.findById(dados.idMedico()).orElseThrow(()-> new RuntimeException("Médico não encontrado"));
-//        }
 
-        Paciente paciente = pacienteRepository.findById(dados.idPaciente()).orElseThrow(()-> new RuntimeException("Paciente não encontrado"));
+        LocalTime horaIncio = LocalTime.of(7, 0);
+        LocalTime horaFinal = LocalTime.of(17, 0);
+
+        Paciente paciente = pacienteRepository.findById(dados.idPaciente())
+                .orElseThrow(()->new RuntimeException("Paciente não encontrado"));
+
+
+        if (dados.data().getDayOfWeek() == DayOfWeek.SATURDAY || dados.data().toLocalTime().isBefore(horaIncio) || dados.data().toLocalTime().isAfter(horaFinal)){
+            throw new RuntimeException("Data e horario da consulta inválidas");
+        }
+        if (dados.data().isBefore(LocalDateTime.now().plusMinutes(30))){
+            throw new RuntimeException("O agendamento deve ser feito com 30 minutos de antecedencia");
+        }
+        if (paciente.getAtivo() == false ){
+            throw new RuntimeException("Registro inativo");
+        }
+
+        for (Consulta consulta : listarTodasConsultas()){
+            if (dados.idPaciente().equals(consulta.getPaciente().getId())){
+                if (dados.data().getDayOfMonth() == (consulta.getData().getDayOfMonth())){
+                    throw new RuntimeException("Paciente já tem uma consulta agendada para esse dia");
+                }
+            }if(dados.idMedico().equals(consulta.getMedico().getId())){
+                if (dados.data().equals(consulta.getData())){
+                    throw new RuntimeException("Médico já tem uma consulta agendada para esse dia");
+                }
+            }
+        }
+
 
         Consulta consulta = Consulta.builder()
                 .medico(escolherMedico(dados))
@@ -44,10 +68,17 @@ public class ConsultaService {
         consultaRepository.save(consulta);
     }
 
-    private Medico escolherMedico(DadosAgendamentoConsulta dados){
+    private List<Consulta> listarTodasConsultas(){
+        return consultaRepository.findAll();
+    }
 
+    private Medico escolherMedico(DadosAgendamentoConsulta dados){
         if (dados.idMedico() != null){
-            return medicoRepository.getReferenceById(dados.idMedico());
+            Medico medico = medicoRepository.getReferenceById(dados.idMedico());
+            if (medico.getAtivo() == false){
+                throw new RuntimeException("Médico inativo");
+            }
+            return medico;
         }
 
         if (dados.especialidade() == null){
